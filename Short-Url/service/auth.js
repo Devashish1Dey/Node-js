a75a7fa595ea7907e9a8fdfeeee252dcc8780e06
Devashish1Dey <devashishdey01@gmail.com>
2026-08-02 19:49:21 +0530

    JWT Authentication in NodeJS

--- Short-Url/service/auth.js
+++ Short-Url/service/auth.js
@@ -1,13 +1,24 @@
+const jwt = require('jsonwebtoken')
+// const sessionIdToUserMap = new Map()
+const secret = 'DevDey$12&'
 
-
-const sessionIdToUserMap = new Map()
-
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user)
+function setUser(user){
+    // sessionIdToUserMap.set(id, user)
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    // return sessionIdToUserMap.get(id)
+    if(!token) return null
+    try {
+        return jwt.verify(token, secret)
+    }
+    catch(error){
+        return null
+    }
 }
 
 module.exports = {
