b9f2a5e6345248908e08f57ccdaea9a34bb5dcd5
Devashish1Dey <devashishdey01@gmail.com>
2026-09-23 23:49:20 +0530

    Completed my Blogging App

--- Blogging/index.js
+++ Blogging/index.js
@@ -8,10 +8,14 @@ const Blog = require('./models/blog')
 const userRoute = require('./routes/user')
 const blogRoute = require('./routes/blog')
 const { checkForAuthenticationCookie } = require("./middlewares/authentication")
-const app = express()
-const PORT = 8000
 
-mongoose.connect('mongodb://localhost:27017/blogify').then((e)=> {console.log("MongoDB Connected")})
+const app = express()
+// const PORT = 8000
+const PORT = process.env.PORT || 8000
+
+// mongoose.connect('mongodb://localhost:27017/blogify')
+mongoose.connect(process.env.MONGO_URL)
+.then((e)=> {console.log("MongoDB Connected")})
 
 app.set('view engine', "ejs")
 app.set("views", path.resolve('./views'))
